3806c625dfb4a07c7ada5645b6ebfac1777ead71
neha01mittal <neha01mittal@gmail.com>
2019-03-31 18:47:53 -0700

    ml integration with dashboard

--- ai-health-code/run.py
+++ ai-health-code/run.py
@@ -1,13 +1,28 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+import pandas as pd 
 
 app = Flask(__name__)
 
 counter = 0
+clf = None
 def start_runner():
     print("Hello")
     #train here
+    df_final = pd.read_csv("clean_data.csv")
+    y = df_final['Species']
 
+    x = df_final.drop(['Species'], axis=1)
+
+    #x_train, x_test, y_train, y_test = train_test_split(x,y, test_size= 0.25, random_state=27)
+    clf = MLPClassifier(hidden_layer_sizes=(100,100,100), max_iter=5, alpha=0.0001,
+                     solver='sgd', verbose=10,  random_state=21,tol=0.000000001)
+    clf.fit(x, y)
+ 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
@@ -25,7 +40,9 @@ def incoming_sms():
     counter += 1
 
     #2. Test format
-
+    x_test = pd.read_csv("test_data.csv")
+    y_pred = clf.predict(x_test)
+    print("Prediction: ", y_pred)  
     return str(resp)
 
 
